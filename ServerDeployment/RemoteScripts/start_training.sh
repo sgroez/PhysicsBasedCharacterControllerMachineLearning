0eb635f52fe6b6ef50577ad2ec43cd59492870b8
Simon Grözinger <simon-groezinger@web.de>
2024-04-09 11:06:18 +0200

    added server side script to start training

---
+++ ServerDeployment/RemoteScripts/start_training.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source .env #load env variables
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate mlagents
+mlagents-learn ./Walker.yaml --env=./Build/Build --run-id=Walker$version --no-graphics --num-envs=$env_count --force #start training
