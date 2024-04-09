9462a336116919ae85910f6e91a548dc9bc80f33
Simon Grözinger <simon-groezinger@web.de>
2024-04-09 08:23:41 +0200

    removed environment spawner using num envs argument instead

--- ServerDeployment/DownloadResults.sh
+++ ServerDeployment/DownloadResults.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 source ../.env
-scp $remote_server_user@$remote_server_address:$remote_results_path ../Project/Assets/remoteResults
+scp -r $remote_server_user@$remote_server_address:$remote_results_path ../Project/Assets/RemoteResults
