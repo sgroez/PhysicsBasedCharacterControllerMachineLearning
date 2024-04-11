eaa88204cd1dc23c323bcfc711b8c5cd91ad6ead
Simon Grözinger <simon-groezinger@web.de>
2024-04-11 15:21:13 +0200

    updated download results script with conditional final flag to only download final training model

--- ServerDeployment/DownloadResults.sh
+++ ServerDeployment/DownloadResults.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 source ../.env
+if [ "$1" == "-final" ]; then
+scp -r $remote_server_user@$remote_server_address:$remote_results_path$version/Walker.onnx ../Project/Assets/RemoteResults/Walker$version.onnx
+else
 scp -r $remote_server_user@$remote_server_address:$remote_results_path$version ../Project/Assets/RemoteResults
+fi
