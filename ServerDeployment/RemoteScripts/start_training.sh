7f19647cb1a0595b86692ea87f90096dfcb47e08
Simon Grözinger <simon-groezinger@web.de>
2024-04-12 12:16:02 +0200

    updated conda env name in start training script

--- ServerDeployment/RemoteScripts/start_training.sh
+++ ServerDeployment/RemoteScripts/start_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env #load env variables
 source ~/anaconda3/etc/profile.d/conda.sh
-conda activate mlagents
+conda activate mlagentsR21
 flags="--env=./Build/Build --run-id=$name$version --no-graphics --num-envs=$env_count"
 if [ "$1" == "-continue" ]; then
 flags+=" --resume"
