c944ac048df8a81fe693bf83385955dd11ae9a96
Simon Grözinger <simon-groezinger@web.de>
2024-04-12 12:09:55 +0200

    added name variable to env to make model names more specific

--- ServerDeployment/DownloadResults.sh
+++ ServerDeployment/DownloadResults.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 source ../.env
 if [ "$1" == "-final" ]; then
-scp -r $remote_server_user@$remote_server_address:$remote_results_path$version/Walker.onnx ../Project/Assets/RemoteResults/Walker$version.onnx
+scp -r $remote_server_user@$remote_server_address:$remote_results_path/$name$version/Walker.onnx ../Project/Assets/RemoteResults/Walker$version.onnx
 else
-scp -r $remote_server_user@$remote_server_address:$remote_results_path$version ../Project/Assets/RemoteResults
+scp -r $remote_server_user@$remote_server_address:$remote_results_path/$name$version ../Project/Assets/RemoteResults
 fi
--- ServerDeployment/RemoteScripts/start_training.sh
+++ ServerDeployment/RemoteScripts/start_training.sh
@@ -2,4 +2,4 @@
 source .env #load env variables
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate mlagents
-mlagents-learn ./Walker.yaml --env=./Build/Build --run-id=Walker$version --no-graphics --num-envs=$env_count --force #start training
+mlagents-learn ./Walker.yaml --env=./Build/Build --run-id=$name$version --no-graphics --num-envs=$env_count --force #start training
