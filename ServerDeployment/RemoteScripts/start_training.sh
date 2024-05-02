5092d561431b9c6c1c2763d7f8440e8ba51fe067
Simon Grözinger <simon-groezinger@web.de>
2024-05-02 10:28:52 +0200

    changed run id format to make it easier to sort in tensorboard

--- ServerDeployment/RemoteScripts/start_training.sh
+++ ServerDeployment/RemoteScripts/start_training.sh
@@ -2,7 +2,7 @@
 source .env #load env variables
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate mlagentsR21
-flags="--env=./Build/Build --run-id=$name$version --no-graphics"
+flags="--env=./Build/Build --run-id=$version_$name --no-graphics"
 if [ "$1" == "-continue" ]; then
 flags+=" --resume"
 else
