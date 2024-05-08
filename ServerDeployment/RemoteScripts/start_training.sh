6200b2e47aafbfba8def17e488ce102a55b3b750
Simon Grözinger <simon-groezinger@web.de>
2024-05-08 09:38:06 +0200

    updated start training script to pass all parameters to mlagents command

--- ServerDeployment/RemoteScripts/start_training.sh
+++ ServerDeployment/RemoteScripts/start_training.sh
@@ -3,9 +3,4 @@ source .env #load env variables
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate mlagentsR21
 flags="--env=./Build/Build --run-id=${version}_$name --no-graphics"
-if [ "$1" == "-continue" ]; then
-flags+=" --resume"
-else
-flags+=" --force"
-fi
-mlagents-learn ./$config_name $flags
+mlagents-learn ./$config_name $flags "$@"
