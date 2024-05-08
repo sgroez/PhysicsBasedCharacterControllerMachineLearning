788492570b0bd4c1a32df466499b4d017e3a562a
Simon Grözinger <simon-groezinger@web.de>
2024-05-08 09:24:10 +0200

    updated download script with new file nameing

--- ServerDeployment/DownloadResults.sh
+++ ServerDeployment/DownloadResults.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 source ../.env
 if [ "$1" == "-final" ]; then
-scp -r $remote_server_user@$remote_server_address:$remote_results_path/${version}_$name/Walker.onnx ../Project/Assets/RemoteResults/$name$version.onnx
+scp -r $remote_server_user@$remote_server_address:$remote_results_path/${version}_$name/Walker.onnx ../Project/Assets/RemoteResults/${version}_$name.onnx
 else
 scp -r $remote_server_user@$remote_server_address:$remote_results_path/${version}_$name ../Project/Assets/RemoteResults
 fi
