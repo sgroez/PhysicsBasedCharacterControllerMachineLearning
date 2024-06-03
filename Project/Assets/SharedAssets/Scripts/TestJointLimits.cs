1e5cebec4a02a84bb18ca5fc0da4156cb71f291a
Simon Grözinger <simon-groezinger@web.de>
2024-06-03 15:16:38 +0200

    improved test joint limits script and setup character to match joint axis from walker demo

--- Project/Assets/SharedAssets/Scripts/TestJointLimits.cs
+++ Project/Assets/SharedAssets/Scripts/TestJointLimits.cs
@@ -4,32 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class ToggleTestJoint
+public class TestJoint
 {
-    public bool enabled;
     public string name;
     public ConfigurableJoint joint;
+    public Vector3 enabledAxis;
+    [Range(0f, 1f)] public float extension;
+    [HideInInspector] public float lastExtension;
 
-    public ToggleTestJoint(bool enabled, string name, ConfigurableJoint joint)
+    public TestJoint(string name, ConfigurableJoint joint)
     {
-        this.enabled = enabled;
         this.name = name;
         this.joint = joint;
+        enabledAxis = Vector3.zero;
+        extension = 0.5f;
+        lastExtension = 0f;
     }
 }
 
 public class TestJointLimits : MonoBehaviour
 {
-    public Vector3 testAxis;
-    public float speed = .8f;
-
     public Rigidbody root;
-    public List<ToggleTestJoint> testJoints = new List<ToggleTestJoint>();
-    private ConfigurableJoint joint;
-    private float lowXLimit;
-    private float highXLimit;
-    private float yLimit;
-    private float zLimit;
+    public List<TestJoint> testJoints = new List<TestJoint>();
 
     void Start()
     {
@@ -39,40 +35,40 @@ public class TestJointLimits : MonoBehaviour
         ConfigurableJoint[] jointsInChildren = GetComponentsInChildren<ConfigurableJoint>();
         foreach (ConfigurableJoint joint in jointsInChildren)
         {
-            testJoints.Add(new ToggleTestJoint(false, joint.gameObject.name, joint));
+            testJoints.Add(new TestJoint(joint.gameObject.name, joint));
         }
     }
 
     void Update()
     {
-        foreach (ToggleTestJoint testJoint in testJoints)
+        foreach (TestJoint testJoint in testJoints)
         {
             UpdateJointRotation(testJoint);
         }
     }
 
-    void UpdateJointRotation(ToggleTestJoint testJoint)
+    void UpdateJointRotation(TestJoint testJoint)
     {
-        if (!testJoint.enabled) return;
+        //reduce duplicate updates
+        if (testJoint.extension == testJoint.lastExtension) return;
 
         //get enabled axis values from input
-        float enabledX = testAxis.x > 0f ? 1f : 0f;
-        float enabledY = testAxis.y > 0f ? 1f : 0f;
-        float enabledZ = testAxis.z > 0f ? 1f : 0f;
-        //get current time * speed
-        float timer = Time.time * speed;
-        //ping pong between 0 and 1 depending on timer
-        float currentPhase = Mathf.PingPong(timer, 1f);
+        float enabledX = testJoint.enabledAxis.x > 0f ? 1f : 0f;
+        float enabledY = testJoint.enabledAxis.y > 0f ? 1f : 0f;
+        float enabledZ = testJoint.enabledAxis.z > 0f ? 1f : 0f;
         //get current joint limits
-        lowXLimit = testJoint.joint.lowAngularXLimit.limit;
-        highXLimit = testJoint.joint.highAngularXLimit.limit;
-        yLimit = testJoint.joint.angularYLimit.limit;
-        zLimit = testJoint.joint.angularZLimit.limit;
+        float lowXLimit = testJoint.joint.lowAngularXLimit.limit;
+        float highXLimit = testJoint.joint.highAngularXLimit.limit;
+        float yLimit = testJoint.joint.angularYLimit.limit;
+        float zLimit = testJoint.joint.angularZLimit.limit;
         //lerp between high and low joint limits depending on current phase
-        float xRot = enabledX * Mathf.Lerp(lowXLimit, highXLimit, currentPhase);
-        float yRot = enabledY * Mathf.Lerp(-yLimit, yLimit, currentPhase);
-        float zRot = enabledZ * Mathf.Lerp(-zLimit, zLimit, currentPhase);
+        float xRot = enabledX * Mathf.Lerp(lowXLimit, highXLimit, testJoint.extension);
+        float yRot = enabledY * Mathf.Lerp(-yLimit, yLimit, testJoint.extension);
+        float zRot = enabledZ * Mathf.Lerp(-zLimit, zLimit, testJoint.extension);
         //set new target rotation
         testJoint.joint.targetRotation = Quaternion.Euler(xRot, yRot, zRot);
+
+        //set new last extension
+        testJoint.lastExtension = testJoint.extension;
     }
 }
