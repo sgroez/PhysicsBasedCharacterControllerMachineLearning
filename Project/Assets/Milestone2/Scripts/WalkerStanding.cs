03811838d88866445387c12041c95f83e3f417a1
Simon Grözinger <simon-groezinger@web.de>
2024-07-26 14:55:13 +0200

    124 added standing still environment

---
+++ Project/Assets/Milestone2/Scripts/WalkerStanding.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class WalkerStanding : WalkerAgent1
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        targetWalkingSpeed = 0f;
+    }
+    public override void FixedUpdate()
+    {
+        Vector3 avgVelocity = GetAvgVelocity();
+        float standingReward = Mathf.Exp(-100 * Mathf.Pow(avgVelocity.magnitude, 2));
+        RecordStat("standingReward", standingReward);
+
+        AddReward(standingReward);
+    }
+}
