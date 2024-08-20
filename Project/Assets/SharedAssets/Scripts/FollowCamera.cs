f7d98950d7edd700954e6966ffee893c33ed2ac0
Simon Grözinger <simon-groezinger@web.de>
2024-08-20 13:10:21 +0200

    improved chapter gangbild anpassungen

--- Project/Assets/SharedAssets/Scripts/FollowCamera.cs
+++ Project/Assets/SharedAssets/Scripts/FollowCamera.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public UserController userController;
+    public Transform followObject;
+    public Orientation orientation;
     public float offset;
 
     void FixedUpdate()
     {
         Vector3 direction = Vector3.forward;
-        if (userController == null)
+        if (userController != null)
         {
-            throw new Exception("Missing User Controller");
-        }
-        else if (userController.worldAxisMode)
-        {
-            direction = Vector3.up;
+            if (userController.worldAxisMode)
+            {
+                direction = Vector3.up;
+            }
+            else
+            {
+                direction = userController.rotation * -Vector3.forward;
+                direction = new Vector3(direction.x, 0f, direction.z);
+            }
+            transform.position = userController.root.position + (direction * offset);
+            transform.LookAt(userController.root);
         }
         else
         {
-            direction = userController.rotation * -Vector3.forward;
-            direction = new Vector3(direction.x, 0f, direction.z);
+            direction = followObject.forward;
+            switch (orientation)
+            {
+                case Orientation.Right:
+                    direction = followObject.right;
+                    break;
+                case Orientation.Left:
+                    direction = -followObject.right;
+                    break;
+                case Orientation.Backward:
+                    direction = -followObject.forward;
+                    break;
+            }
+            direction.y = 0f;
+            transform.position = followObject.position + (direction * offset);
+            transform.LookAt(followObject.position);
         }
-        transform.position = userController.root.position + (direction * offset);
-        transform.LookAt(userController.root);
     }
 }
